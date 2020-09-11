b1d03404370fe1be71667f46f4cea417b0f0b4c6
Faizii35 <faizii.3406@gmail.com>
2020-09-12 03:22:27 +0500

    Fragments ( Profile & Edit Profile) Created And Profile Activity Deleted

--- app/src/main/java/com/it/partaker/activities/MainActivity.kt
+++ app/src/main/java/com/it/partaker/activities/MainActivity.kt
@@ -6,11 +6,12 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
-import androidx.fragment.app.Fragment
 import androidx.navigation.ui.AppBarConfiguration
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
@@ -18,7 +19,6 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
-import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.StorageTask
@@ -46,8 +46,10 @@ class MainActivity : AppCompatActivity() {
         userReference = FirebaseDatabase.getInstance().reference.child("users").child(firebaseUser?.uid.toString())
         storageRef = FirebaseStorage.getInstance().reference.child("User Images")
 
-//        val userId = FirebaseAuth.getInstance().currentUser!!.uid
-//        val userRef = FirebaseDatabase.getInstance().reference.child("users").child(userId)
+        val spinBlood = findViewById<Spinner>(R.id.spnEditProfileBloodGroup)
+        val bloodGroup = arrayOf("Not Known","A+","A-","B+","B-","AB+","AB-","O+","O-")
+        val ArrayAdp = ArrayAdapter(this@MainActivity, android.R.layout.simple_spinner_dropdown_item, bloodGroup)
+        spinBlood.adapter = ArrayAdp
 
 
         //Data Retrieval From Firebase in Profile Fragment
